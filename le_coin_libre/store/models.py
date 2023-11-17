6491b49d693042646c4cdaabadd6b962adae05c2
Morvan Titouan <titouan.morvan@student-cs.fr>
2023-11-17 15:27:24 +0100

    fix product

--- le_coin_libre/store/models.py
+++ le_coin_libre/store/models.py
@@ -28,5 +28,3 @@ class Product(models.Model):
 class Image(models.Model):
     image = models.ImageField(upload_to='static/images/',default='static/images/No-img.jpg')
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    
-    
