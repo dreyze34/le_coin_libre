ebb95db53fb7cb4ec9a90ec872b6573dc959f013
Benyahia Mathis <mathis.benyahia@student-cs.fr>
2023-11-21 19:14:13 +0100

    modification du style page produit et restructuration du CSS

--- le_coin_libre/store/models.py
+++ le_coin_libre/store/models.py
@@ -3,6 +3,7 @@ from datetime import date
 from django.db import models
 from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
+from unidecode import unidecode 
 
 
 
