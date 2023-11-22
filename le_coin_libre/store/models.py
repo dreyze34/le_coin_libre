3f78f921b7f3bc9cebae564ed660b49376427951
Benyahia Mathis <mathis.benyahia@student-cs.fr>
2023-11-22 09:52:54 +0100

    ajout de la fonctionnalité de l'heure

--- le_coin_libre/store/models.py
+++ le_coin_libre/store/models.py
@@ -23,7 +23,7 @@ class Product(models.Model):
     title = models.CharField(max_length=200)
     description = models.CharField(max_length=500)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    date = models.DateField(default=date.today())
+    date = models.DateTimeField(default=timezone.now)
     user = models.ForeignKey(UserProfile, on_delete=models.CASCADE, default=None)
     category = models.ForeignKey(Category, on_delete = models.CASCADE)
     normalized_title=models.CharField(max_length=200, default="default")
